8f063704da2b199991594e989a22d55248cab391
Matthias Southwick <33238125+Mythius@users.noreply.github.com>
2023-08-05 15:52:33 -0600

    added speaking

--- server.js
+++ server.js
@@ -51,18 +51,65 @@ http.listen(port,()=>{console.log('Serving Port: '+port)});
 
 io.on('connection',socket=>{
 	var c = new client(socket);
+	socket.on('getlist',words=>{
+		let word1 = words[0];
+		let all_lists = [];
+		for(let word of words){
+			all_lists.push(wordToList(word));
+		}
+		Promise.all(all_lists).then(values=>{
+			c.emit('list',values);
+		});
+	})
 });
 
+var sounds;
+file.read('sounds.json',data=>{sounds=JSON.parse(data)});
+
+async function wordToList(word){
+	let pron = await getPronunciationWEB(word);
+	let paths = convert_to_paths(pron);
+	return paths;
+}
 
 async function main(){
 	let word = await c.in('Enter word: ');
 	let pron = await getPronunciationWEB(word);
 	console.log(pron);
+	let paths = convert_to_paths(pron);
+	console.log(paths);
 	main();
 }
 
-main();
 
+// main();
+
+function convert_to_paths(pron){
+	// console.log(`Sounds:${sounds.join(',')}`);
+	let result = [];
+	for(let i=0;i<pron.length;i++){
+		if(pron[i]=='ˈ'||pron[i]==' ') continue;
+		if(sounds.indexOf(pron[i])!=-1){
+			result.push(`sounds/${pron[i]}.wav`);
+		} else {
+			let c = false;
+			for(let j=i;j<pron.length;j++){
+				let sub = pron.slice(i,j+1);
+				console.log(`Tried:`+sub);
+				if(sounds.indexOf(sub)!=-1){
+					result.push(`sounds/${sub}.wav`);
+					c = true;
+					i=j;
+					break;
+				}
+			}
+			if(!c){
+				console.log("Couldn't Find:"+pron.slice(i));
+			}
+		}
+	}
+	return result;
+}
 
 function getPronunciationWEB(word){
 	return new Promise((res,rej)=>{
