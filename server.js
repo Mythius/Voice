f4e7c118b747e33bbe1e1dc1f0099a4d1333d039
Matthias Southwick <southwickmatthias@gmail.com>
2023-08-05 17:38:48 -0600

    Update sounds and saved pronunciations list

--- server.js
+++ server.js
@@ -57,6 +57,7 @@ io.on('connection',socket=>{
 		for(let word of words){
 			all_lists.push(wordToList(word));
 		}
+		// console.log('');
 		Promise.all(all_lists).then(values=>{
 			c.emit('list',values);
 		});
@@ -69,6 +70,7 @@ file.read('sounds.json',data=>{sounds=JSON.parse(data)});
 async function wordToList(word){
 	let pron = await getPronunciationWEB(word);
 	let paths = convert_to_paths(pron);
+	console.log(pron);
 	return paths;
 }
 
@@ -82,36 +84,46 @@ async function main(){
 }
 
 
-// main();
+main();
 
 function convert_to_paths(pron){
 	// console.log(`Sounds:${sounds.join(',')}`);
+	pron = pron.replaceAll(' ','_');
 	let result = [];
 	for(let i=0;i<pron.length;i++){
-		if(pron[i]=='ˈ'||pron[i]==' ') continue;
-		if(sounds.indexOf(pron[i])!=-1){
-			result.push(`sounds/${pron[i]}.wav`);
-		} else {
-			let c = false;
-			for(let j=i;j<pron.length;j++){
-				let sub = pron.slice(i,j+1);
-				console.log(`Tried:`+sub);
-				if(sounds.indexOf(sub)!=-1){
-					result.push(`sounds/${sub}.wav`);
-					c = true;
-					i=j;
-					break;
-				}
+	if(pron[i]=='ˈ'||pron[i]=='ˌ'||pron[i]=='_') continue;
+		let c = false;
+		let j=pron.length;
+		while(j>=i){
+			let sub = pron.slice(i,j+1);
+			// console.log(`Tried:`+sub);
+			if(sounds.indexOf(sub)!=-1){
+				result.push(`sounds/${sub}.wav`);
+				c = true;
+				i=j;
+				break;
 			}
-			if(!c){
-				console.log("Couldn't Find:"+pron.slice(i));
-			}
+			j--;
+		}
+		for(let j=i+1;j<pron.length;j++){
+		}
+		if(!c){
+			console.log("Couldn't Find:"+pron.slice(i));
 		}
 	}
+	result.push('sounds/_.wav');
 	return result;
 }
 
+
+var saved_prons;
+file.read('saved_pronunciations.json',data=>{saved_prons=JSON.parse(data)});
+
 function getPronunciationWEB(word){
+	word = word.toLowerCase();
+	if(word in saved_prons){
+		return saved_prons[word];
+	}
 	return new Promise((res,rej)=>{
 		let proc = system.spawn('curl',[`https://www.dictionary.com/browse/${word.trim()}`]);
 		proc.stdout.on('data',data=>{
@@ -121,7 +133,9 @@ function getPronunciationWEB(word){
 				let ix = str.indexOf(';');
 				if(ix==-1) str = str.split(';')[0];
 				str = str.slice(0,str.indexOf(',')).replaceAll(/('|")/gi,'').replaceAll(/\\[^>]+>/g,'').trim();
-				str = str.replaceAll(/;.+/g,'');
+				str = str.replaceAll(/;.+/g,'').replaceAll(/stressed/g,'').trim();
+				saved_prons[word]=str;
+				file.save('saved_pronunciations.json',JSON.stringify(saved_prons))
 				res(str);
 				return;
 			}
